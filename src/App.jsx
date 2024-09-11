76ab9ae7060cd8f24f63d563a0afca2af9d32744
Windisley <windisleyl@gmail.com>
2024-09-11 19:23:43 -0300

    Css global definido

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Styles from "../src/App.module.css"
+
+function App() {
+
+  return (
+    <section>
+        <div className={Styles.containerstopwatch}>
+           <div className={Styles.boxstopWatch}>
+
+           </div>
+        </div>
+    </section>
+  )
+}
+
+export default App
