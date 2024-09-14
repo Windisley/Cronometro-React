bff454372f0546ab986776a1d453e0fab34c0293
Windisley <windisleyl@gmail.com>
2024-09-14 11:51:21 -0300

    logica do Stopwatch comcluida

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Styles from "../src/App.module.css"
 import { GrPowerReset } from "react-icons/gr";
 import { FaPlay } from "react-icons/fa";
-
+import { FaPause } from "react-icons/fa";
 
 function App(){
  
@@ -10,9 +10,64 @@ function App(){
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [isruning, setIsruning] = useState(false)
+  
+  let interval = null
+  useEffect(()=>{
+   if(isruning){
+     interval = setInterval(()=>{
+      
+     setSeconds((second)=>{
 
+       if(second > 99){
+         second = 0
 
+         setMinutes((minute)=>{
+           if(minute > 59){
+             minute = 0
+            
+             setHours((hour)=>{
+                 return hour+=1
+             })
+           }
+           return minute+=1
 
+           
+         })
+
+       }
+       
+       return second+=1
+      })
+      
+      }, 10)
+      
+    }else{
+      
+      clearInterval(interval)
+      
+    }
+    
+    
+    return ()=> clearInterval(interval)
+    
+    
+  }, [isruning])
+  
+  
+  function activelogic(){
+    setIsruning(thenclick => !thenclick)
+    
+  }
+
+  function resetstopwatch(){
+    clearInterval(interval)
+    setIsruning(IsRuning=> IsRuning = false)
+    setSeconds(0)
+    setMinutes(0)
+    setHours(0)
+  }
+  
+  
   return (
     <section>
       <div className={Styles.containerstopwatch}>
@@ -21,9 +76,9 @@ function App(){
           <div className={Styles.absolut}>
             <p id={Styles.hours}>
               {
-
+                
                 hours < 10 ? "0" + hours : hours
-
+                
               }
             </p>
             <span className={Styles.point}>
@@ -47,10 +102,10 @@ function App(){
 
         <div className={Styles.options}>
           <div>
-            <GrPowerReset className={Styles.iconswatch} title='Resetar' />
+            <GrPowerReset className={Styles.iconswatch} title='Resetar'  onClick={resetstopwatch}/>
           </div>
           <div>
-            <FaPlay className={Styles.iconswatch} title='Começar'/>
+            {isruning === true ? <FaPause className={Styles.iconswatch} title='pausar' onClick={activelogic}/> : <FaPlay className={Styles.iconswatch} title='começar' onClick={activelogic}/>}
           </div>
         </div>
       </div>
